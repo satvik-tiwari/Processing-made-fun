e517d853363394fdd4e079dc39698c94319202e2
Kivtas <31216604+satvik-tiwari@users.noreply.github.com>
2019-03-06 01:40:24 +0530

    Create sketch.java

---
+++ Perlin Noise/1D_Perlin_Noise/sketch.java
@@ -0,0 +1,24 @@
+float start = 0;
+void setup()
+{
+  size(400,400);
+  
+}
+
+void draw()
+{
+  float xoff = start;
+  background(51);
+  stroke(255);
+  noFill();
+  beginShape();
+  for(int x = 0; x < width; x++)
+  {
+   stroke(255); 
+   float y = map(noise(xoff), 0, 1, 0, height);
+   vertex(x, y);
+   xoff += 0.02;
+  }
+  endShape();
+  start += 0.02;
+} 
